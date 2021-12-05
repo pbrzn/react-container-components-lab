69ef11338a0dec8ca0c5ad148d97eb1142b1c90d
Patrick Brennan <pbrennanmusic@gmail.com>
2021-12-05 16:41:23 -0500

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,15 @@
-// Code MovieReviews Here
+import React from "react";
+
+const MovieReviews = ({ reviews }) => (
+  <div className="review-list">
+    {reviews.map((review) => (
+      <div className="review">
+        <img src={review.multimedia.src} alt={review.display_title} />
+        <h1>{review.display_title}</h1>
+        <p>{review.summary_short}</p>
+      </div>
+    ))}
+  </div>
+)
+
+export default MovieReviews
